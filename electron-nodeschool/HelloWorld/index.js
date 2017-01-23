cde1ebcf755175f0ce87d8499ae3f05baa68d4cd
Jose Medina <josemedina921@gmail.com>
2017-01-22 22:39:24 -0800

    save a pdf when pressing p

--- electron-nodeschool/HelloWorld/index.js
+++ electron-nodeschool/HelloWorld/index.js
@@ -1,5 +1,7 @@
 const picture = require('cat-picture');
 const image = require('lightning-image-poly');
+const {remote} = require('electron');
+const fs = require('fs');
 
 const { src } = picture;
 
@@ -7,4 +9,17 @@ picture.remove();
 
 var viz = new image('#visualization',null, [src], {hullAlgorithm: 'convex'});
 
+function save () {
+  remote.getCurrentWindow().webContents.printToPDF({ portrait: true }, 
+  function(err, data) {
+    fs.writeFile('annotation.pdf', data, function(err) {
 
+      if(err) alert('error generating pdf! ' + err.message);
+      else alert('pdf saved!');
+    })
+  })
+}
+
+window.addEventListener('keydown', function(e) {
+  if(e.keyCode === 80) save()
+})
