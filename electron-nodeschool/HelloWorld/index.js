818c70d294ed519287a005aa255ee35c29f73310
Jose Medina <josemedina921@gmail.com>
2017-01-22 22:19:47 -0800

    add some stupid lighting effect

--- electron-nodeschool/HelloWorld/index.js
+++ electron-nodeschool/HelloWorld/index.js
@@ -1,1 +1,10 @@
-require('cat-picture');
+const picture = require('cat-picture');
+const image = require('lightning-image-poly');
+
+const { src } = picture;
+
+picture.remove();
+
+var viz = new image('#visualization',null, [src], {hullAlgorithm: 'convex'});
+
+
