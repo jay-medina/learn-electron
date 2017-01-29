76d2b1d73b231ae594814ff80cb4b711202d1555
Jose Medina <josemedina921@gmail.com>
2017-01-29 15:14:25 -0800

    getting the sounds to play

---
+++ sound-machine/app/js/index.js
@@ -0,0 +1,25 @@
+
+
+const soundMachineBtns = document.querySelectorAll('.sound-machine--buttons');
+const audioFiles = {};
+
+function playAudio(filename) {
+    if(!audioFiles[filename]) {
+      audioFiles[filename] = new Audio(`wav/${filename}.wav`);
+    }
+     audioFiles[filename].currentTime = 0;
+     audioFiles[filename].play();
+}
+function onClick(e) {
+    const target = e.target;
+    let sound;
+
+    if(target.dataset.sound) {
+        sound = target.dataset.sound;
+        console.log(sound);
+        playAudio(sound);
+    }
+}
+
+soundMachineBtns.forEach(btn => btn.addEventListener('click', onClick));
+
