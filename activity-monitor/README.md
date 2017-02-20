925a835f529612cbb1ea90eb888049eafd585905
Jose Medina <josemedina921@gmail.com>
2017-02-19 21:42:03 -0800

    Add readme and complete activity

---
+++ activity-monitor/README.md
@@ -0,0 +1,24 @@
+# Activity Monitor
+Implementing The Activity Monitor from [Electron's Simple Samples](http://electron.atom.io/blog/2017/01/19/simple-samples).
+
+Activity Monitor uses Electron and Chart.js library.
+
+## To Install
+```bash
+# Install Yarn through brew
+brew install yarn 
+
+#Or install yarn through npm
+npm install yarn -g
+
+#install all dependencies
+yarn
+
+#run the app
+npm start
+
+```
+
+## Links to sources
+- Chart.js: http://www.chartjs.org/
+- [Electron's Simple Samples: http://electron.atom.io/blog/2017/01/19/simple-samples
--- activity-monitor/src/renderer/index.js
+++ activity-monitor/src/renderer/index.js
@@ -1,39 +1,49 @@
+var os = require('os')
 var Chart = require('chart.js')
 var ctx = document.querySelector('.chart');
 
+const WHITE = '#FFF';
+const cpus = os.cpus()
+
+const datasets = cpus.map(function(cpu){
+  return {
+    data: [
+      cpu.times.user,
+      cpu.times.sys,
+      cpu.times.idle
+    ],
+    backgroundColor: [
+      'rgba(255, 99, 132, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)'
+    ]
+  };
+});
+
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+  type: 'doughnut',
+  data: {
+    datasets,
+    labels: [
+      'User Time (ms)',
+      'System Time (ms)',
+      'Idle Time (ms)'
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    title: {
+     text: 'CPU Activity',
+     display: true,
+     fontSize: 16,
+     fontColor: WHITE
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
+    legend: {
+      display: true,
+      labels: {
+        fontColor: 'rgb(250, 250, 250)',
+        fontSize: 12
+      }
     }
+  }
 });
